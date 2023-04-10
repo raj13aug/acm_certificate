92cad4564c57fba93ed2971e2e39149629c31c23
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-04-10 15:38:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+# ********************************* #
+# * ACM *                           #
+# ********************************* #
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+variable "root_domain_name" {
+  type    = string
+  default = "robofarming.link"
+}
+
+data "aws_route53_zone" "certificate_route53_zone" {
+  name         = var.root_domain_name
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "certificate" {
+  domain_name               = var.certificate_dns_name
+  subject_alternative_names = ["*.${var.certificate_dns_name}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_dns" {
+  for_each = {
+    for robo in aws_acm_certificate.certificate.domain_validation_options : robo.domain_name => {
+      name   = robo.resource_record_name
+      record = robo.resource_record_value
+      type   = robo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.certificate_route53_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "certificate" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_dns : record.fqdn]
+}
